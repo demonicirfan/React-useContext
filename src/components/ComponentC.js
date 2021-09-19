8b820711417566b76623d6ab494ad7e0e710c080
Irfan Asif <hashtag.irfan@gmail.com>
2021-09-19 15:01:50 +0530

    🎓

---
+++ src/components/ComponentC.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const ComponentC = () => {
+    return (
+        <div>
+            ComponentC  
+        </div>
+    )
+}
+
+export {ComponentC}
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export * from './ComponentA'
+export * from './ComponentB'
+export * from './ComponentC'
+export * from './ComponentD'
+export * from './ComponentE'
+export * from './ComponentF'
